25d6af055413135836f2684cbe0b7309f8366ab4
antonu <ytkaev12@live.com>
2025-06-26 01:02:19 +0300

    drop fix

--- src/main/java/ru/ancndz/bandagez/item/GrassDropModifier.java
+++ src/main/java/ru/ancndz/bandagez/item/GrassDropModifier.java
@@ -5,16 +5,12 @@ import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.storage.loot.LootContext;
-import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
 import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
 import net.minecraftforge.common.loot.IGlobalLootModifier;
 import net.minecraftforge.common.loot.LootModifier;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Optional;
 import java.util.function.Supplier;
 
 public class GrassDropModifier extends LootModifier {
@@ -31,10 +27,7 @@ public class GrassDropModifier extends LootModifier {
 	@Override
 	protected @NotNull ObjectArrayList<ItemStack> doApply(ObjectArrayList<ItemStack> generatedLoot,
 			LootContext context) {
-		if (Optional.ofNullable(context.getParamOrNull(LootContextParams.BLOCK_STATE)).map(BlockState::getBlock)
-				.map(Blocks.GRASS::equals).orElse(Boolean.FALSE)) {
-			generatedLoot.add(new ItemStack(Items.FLORAL_STRING_ITEM.get()));
-		}
+        generatedLoot.add(new ItemStack(Items.FLORAL_STRING_ITEM.get()));
 		return generatedLoot;
 	}
 
