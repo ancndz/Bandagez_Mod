9a32e32d6cc4347586e362776505ac3bbdf9e24d
antonu <ytkaev12@live.com>
2025-07-03 23:28:38 +0300

    config small fix

--- common/src/main/java/ru/ancndz/bandagez/config/ServerModConfiguration.java
+++ common/src/main/java/ru/ancndz/bandagez/config/ServerModConfiguration.java
@@ -1,6 +1,10 @@
 package ru.ancndz.bandagez.config;
 
 public abstract class ServerModConfiguration<T> extends ModConfiguration<T> {
+
+    private static final class Options {
+    }
+
     public ServerModConfiguration() {
         super();
         // Server configuration can be initialized here if needed
