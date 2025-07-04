7f099c1ef3f77e5c81a3b7480c28820d6eab3852
antonu <ytkaev12@live.com>
2025-07-04 10:45:32 +0300

    compile fix

--- forge/src/main/java/ru/ancndz/bandagez/ForgeConfigurationHandler.java
+++ forge/src/main/java/ru/ancndz/bandagez/ForgeConfigurationHandler.java
@@ -1,15 +1,13 @@
 package ru.ancndz.bandagez;
 
+import java.util.function.Function;
 import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.apache.commons.lang3.tuple.Pair;
 import ru.ancndz.bandagez.config.ClientModConfiguration;
 import ru.ancndz.bandagez.config.ConfigEntry;
 import ru.ancndz.bandagez.config.ModConfiguration;
 import ru.ancndz.bandagez.config.ServerModConfiguration;
-
-import java.util.function.Function;
 
 public class ForgeConfigurationHandler {
 
@@ -19,46 +17,48 @@ public class ForgeConfigurationHandler {
     }
 
     private static ForgeConfigSpec buildClientForgeConfigSpec() {
-        final Pair<ClientModConfiguration<ForgeConfigSpec.ConfigValue<?>>, ForgeConfigSpec> specPair =
-            new ForgeConfigSpec.Builder()
-                .configure(builder -> new ClientModConfiguration<>() {
-                    @Override
-                    public Function<ForgeConfigSpec.ConfigValue<?>, Object> getConverter() {
-                        return ForgeConfigSpec.ConfigValue::get;
-                    }
+        final ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
+        final var config = new ClientModConfiguration<ForgeConfigSpec.ConfigValue<?>>() {
 
-                    @Override
-                    protected Function<ConfigEntry<?>, ForgeConfigSpec.ConfigValue<?>> getValueConverter() {
-                        return getForgeValueConverter(builder);
-                    }
-                });
-        ModConfiguration.setClientConfig(specPair.getLeft());
-        return specPair.getRight();
+            @Override
+            public Function<ForgeConfigSpec.ConfigValue<?>, Object> getConverter() {
+                return ForgeConfigSpec.ConfigValue::get;
+            }
+
+            @Override
+            protected Function<ConfigEntry<?>, ForgeConfigSpec.ConfigValue<?>> getValueConverter() {
+                return getForgeValueConverter(builder);
+            }
+        };
+
+        ModConfiguration.setClientConfig(config);
+        return builder.build();
     }
-    
-    private static ForgeConfigSpec buildServerForgeConfigSpec() {
-        final Pair<ServerModConfiguration<ForgeConfigSpec.ConfigValue<?>>, ForgeConfigSpec> specPair =
-            new ForgeConfigSpec.Builder()
-                .configure(builder -> new ServerModConfiguration<>() {
-                    @Override
-                    public Function<ForgeConfigSpec.ConfigValue<?>, Object> getConverter() {
-                        return ForgeConfigSpec.ConfigValue::get;
-                    }
 
-                    @Override
-                    protected Function<ConfigEntry<?>, ForgeConfigSpec.ConfigValue<?>> getValueConverter() {
-                        return getForgeValueConverter(builder);
-                    }
-                });
-        ModConfiguration.setServerConfig(specPair.getLeft());
-        return specPair.getRight();
+    private static ForgeConfigSpec buildServerForgeConfigSpec() {
+        final ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
+        final var config = new ServerModConfiguration<ForgeConfigSpec.ConfigValue<?>>() {
+
+            @Override
+            public Function<ForgeConfigSpec.ConfigValue<?>, Object> getConverter() {
+                return ForgeConfigSpec.ConfigValue::get;
+            }
+
+            @Override
+            protected Function<ConfigEntry<?>, ForgeConfigSpec.ConfigValue<?>> getValueConverter() {
+                return getForgeValueConverter(builder);
+            }
+        };
+
+        ModConfiguration.setServerConfig(config);
+        return builder.build();
     }
 
 
     private static Function<ConfigEntry<?>, ForgeConfigSpec.ConfigValue<?>> getForgeValueConverter(ForgeConfigSpec.Builder builder) {
         return configEntry -> {
             var configValue = builder.comment(configEntry.getComment())
-                .translation(configEntry.getTranslation());
+                    .translation(configEntry.getTranslation());
             if (configEntry.getRange() != null) {
                 Object value = configEntry.getValue();
                 if (value instanceof Integer) {
