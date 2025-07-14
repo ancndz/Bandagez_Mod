d1259cf4adcbf1c2a4e4bef52944ef89f15e1e9c
antonu <ytkaev12@live.com>
2025-07-14 18:28:52 +0300

    config refactor

--- forge/src/main/java/ru/ancndz/bandagez/ForgeConfigurationHandler.java
+++ forge/src/main/java/ru/ancndz/bandagez/ForgeConfigurationHandler.java
@@ -1,6 +1,5 @@
 package ru.ancndz.bandagez;
 
-import java.util.function.Function;
 import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -21,16 +20,14 @@ public class ForgeConfigurationHandler {
         final Pair<ClientModConfiguration<ForgeConfigSpec.ConfigValue<?>>, ForgeConfigSpec> specPair =
                 new ForgeConfigSpec.Builder()
                         .configure(builder -> new ClientModConfiguration<>() {
-                            final ForgeConfigConverter converter = new ForgeConfigConverter(builder);
-
                             @Override
                             protected <V extends Comparable<? super V>> V getValue(String path) {
-                                return converter.<V>toValue().apply(getValueRaw(path));
+                                return ForgeConfigConverter.<V>toValue().apply(getValueRaw(path));
                             }
 
                             @Override
                             protected <V extends Comparable<? super V>> void putValue(ConfigEntry<V> configEntry) {
-                                getValues().put(configEntry.getPath(), converter.<V>toConfigImplValue().apply(configEntry));
+                                getValues().put(configEntry.getPath(), ForgeConfigConverter.<V>toConfigImplValue(builder).apply(configEntry));
                             }
                         });
         ModConfiguration.setClientConfig(specPair.getLeft());
@@ -41,32 +38,19 @@ public class ForgeConfigurationHandler {
         final Pair<ServerModConfiguration<ForgeConfigSpec.ConfigValue<?>>, ForgeConfigSpec> specPair =
                 new ForgeConfigSpec.Builder()
                         .configure(builder -> new ServerModConfiguration<>() {
-                            final ForgeConfigConverter converter = new ForgeConfigConverter(builder);
-
                             @Override
                             protected <V extends Comparable<? super V>> V getValue(String path) {
-                                return converter.<V>toValue().apply(getValueRaw(path));
+                                return ForgeConfigConverter.<V>toValue().apply(getValueRaw(path));
                             }
 
                             @Override
                             protected <V extends Comparable<? super V>> void putValue(ConfigEntry<V> configEntry) {
-                                getValues().put(configEntry.getPath(), converter.<V>toConfigImplValue().apply(configEntry));
+                                getValues().put(configEntry.getPath(), ForgeConfigConverter.<V>toConfigImplValue(builder).apply(configEntry));
                             }
                         });
         ModConfiguration.setServerConfig(specPair.getLeft());
         return specPair.getRight();
     }
 
-
-    private static <V extends Comparable<? super V>> Function<ConfigEntry<V>, ForgeConfigSpec.ConfigValue<V>> getForgeValueConverter(ForgeConfigSpec.Builder builder) {
-        return configEntry -> {
-            var configValue = builder.comment(configEntry.getComment())
-                    .translation(configEntry.getTranslation());
-            if (configEntry.getRange() != null) {
-                return configValue.defineInRange(configEntry.getPath(), configEntry.getValue(), configEntry.getRange().getMinimum(), configEntry.getRange().getMaximum(), configEntry.getValueClass());
-            }
-            return configValue.define(configEntry.getPath(), configEntry.getValue());
-        };
-    }
 }
 
